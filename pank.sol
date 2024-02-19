828154bcfdc08b8bdf7ad61edc6f26c1912510d1
pankajsh5 <120113677+pankajsh5@users.noreply.github.com>
2024-02-19 22:50:30 +0530

    Update pank.sol

--- pank.sol
+++ pank.sol
@@ -1,1 +1,36 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract FreeFire is ERC20, Ownable(msg.sender) {
+
+    uint public constant Coupen = 10;
+    mapping(address => bool) private CoupenRedeemed;
+
+
+    constructor() ERC20("FreeFire", "FF") {
+        _mint(msg.sender, Coupen);
+    }
+
+    function mintingLoot(address account, uint amount) public onlyOwner {
+        _mint(account, amount);
+    }
+
+    function redeemingLoot() public {
+
+        require(balanceOf(msg.sender) >= Coupen, "Balance is low , plz recharge");
+
+        require(!CoupenRedeemed[msg.sender], "Coupen redeemed already , plz enter valid coupen");
+
+        CoupenRedeemed[msg.sender] = true;
+        _burn(msg.sender, Coupen);
+    }
+
+   function burningLoot(uint amount) public {
+        _burn(msg.sender, amount);
+    }
+
+}
