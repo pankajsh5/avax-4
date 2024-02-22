7ce9600648e884cab3998902b591a00de57e450c
pankajsh5 <120113677+pankajsh5@users.noreply.github.com>
2024-02-22 13:35:09 +0530

    Update FreeFire.sol

--- FreeFire.sol
+++ FreeFire.sol
@@ -1,4 +1,3 @@
-
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
@@ -7,30 +6,36 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract FreeFire is ERC20, Ownable(msg.sender) {
 
-    uint public constant Coupen = 10;
-    mapping(address => bool) private CoupenRedeemed;
+    uint256 killPoints;
+    uint256 coins;
+    uint256 bullets;
 
 
     constructor() ERC20("FreeFire", "FF") {
-        _mint(msg.sender, Coupen);
+        _mint(msg.sender, 10);
+        coins = 10;
+        killPoints = 0;
+        bullets = 1;
     }
 
-    function mintingLoot(address account, uint amount) public onlyOwner {
-        _mint(account, amount);
+    function redeemKillPoint() public {
+        require(killPoints>0,"No kill points to redeem");
+        coins += killPoints;
+        _mint(msg.sender,killPoints);
+        killPoints = 0;
     }
 
-    function redeemingLoot() public {
-
-        require(balanceOf(msg.sender) >= Coupen, "Balance is low , plz recharge");
-
-        require(!CoupenRedeemed[msg.sender], "Coupen redeemed already , plz enter valid coupen");
-
-        CoupenRedeemed[msg.sender] = true;
-        _burn(msg.sender, Coupen);
+    function buyAmmo(uint256 amt ) public {
+        require(coins>=amt,"amout should be less than coins");
+        coins-=amt;
+        _burn(msg.sender,amt);
+        bullets += amt;
     }
 
-   function burningLoot(uint amount) public {
-        _burn(msg.sender, amount);
+   function shootEnemy() public {
+        require(bullets>0, "At least one bullet required to kill enemy");
+        bullets--;
+        killPoints += 2;
     }
 
 }
